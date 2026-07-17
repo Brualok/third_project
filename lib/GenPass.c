dd616f3668790efa6a60c207ff6e777edd8dba77
Bryalok <vasya.zalupin.77@mail.ru>
2026-07-18 00:00:47 +0500

    implementation of GetAlphabet

--- lib/GenPass.c
+++ lib/GenPass.c
@@ -5,3 +5,26 @@ GenPass.c - Реализация функций для генерации паролей
 МК-101
 */
 #include "GenPass.h"
+
+char* GetAlphabet(const char* enteredSymbols) {
+    if (enteredSymbols == NULL || strlen(enteredSymbols) == 0) {
+        return NULL;
+    }
+    char* result = (char*)malloc(strlen(enteredSymbols) + 1);
+    if (result == NULL) return NULL;
+    strcpy(result, enteredSymbols);
+    return result;
+}
+
+
+char* GeneratePassword(int minLen, int maxLen, int len, const char* alphabet) {
+    char* dummy = (char*)malloc(1);
+    if (dummy) dummy[0] = '\0';
+    return dummy;
+}
+
+
+bool CheckFlags_and_GeneratePasswords(int argc, char** argv) {
+    printf("CheckFlags_and_GeneratePasswords called with %d args\n", argc);
+    return true;
+}
