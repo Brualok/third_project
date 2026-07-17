9b37a1a37f5001a7111c7abd30036acf4d07faa6
Bryalok <vasya.zalupin.77@mail.ru>
2026-07-17 21:04:34 +0500

    create structure

--- include/GenPass.h
+++ include/GenPass.h
@@ -4,3 +4,10 @@ GenPass.h - Заголовочный файл для генератора паролей
 Терземан Андрей Александрович
 МК-101
 */
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
--- src/main.c
+++ src/main.c
@@ -4,3 +4,9 @@ main.c - Точка входа в программу генерации паролей
 Терземан Андрей Александрович
 МК-101
 */
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    printf("Password Generator\n");
+    return 0;
+}
